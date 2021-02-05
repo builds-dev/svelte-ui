401afbbc21e0a5f972492e7753b1dbe9816c9931
Johnny Hauser <johnnyhauser@gmail.com>
2021-02-05 15:39:52 -0600

    bugfix: inner node of layout container needs height: 100% to mach parent height in some cases

--- src/Layout.svelte
+++ src/Layout.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Element from './Element.svelte'
 	import { layout, layout_style as layout_any_style, spacing_context } from './layout'
-	import Spacing_context, { get_spacing_context } from './Spacing_context.svelte'
+	import Spacing_context from './Spacing_context.svelte'
 
 	export let ref
 	export let layout_class
@@ -10,13 +10,7 @@
 	export let spacing_x = 0
 	export let spacing_y = 0
 
-	/* const { context_spacing_x, context_spacing_y } = get_spacing_context() */
 	$: spacings = { x: spacing_x || spacing, y: spacing_y || spacing }
-
-/* <Spacing_context */
-/* 	x={ $context_spacing_x - spacings.x } */
-/* 	y={ $context_spacing_y- spacings.y } */
-/* > */
 </script>
 
 <Element
@@ -25,7 +19,7 @@
 >
 	<div
 		class="{ layout_class } { layout }"
-		style="{ layout_style($$props) } { layout_any_style($$props) } { spacing_context(spacings) }"
+		style="height: 100%; { layout_style($$props) } { layout_any_style($$props) } { spacing_context(spacings) }"
 	>
 		<Spacing_context {...spacings}>
 			<slot/>
