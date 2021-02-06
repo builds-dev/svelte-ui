defcf434cf02cc7cffd3f8c9532f9900d35e6af7
Johnny Hauser <johnnyhauser@gmail.com>
2021-02-05 19:06:59 -0600

    remove no longer used reset class

--- src/Nearby.svelte
+++ src/Nearby.svelte
@@ -1,10 +1,9 @@
 <script>
 	import { nearby, nearby_container } from './nearby'
-	import { reset } from './reset'
 </script>
 
 <div class="{ nearby } { $$props.class }">
-	<div class="{ reset } { nearby_container }">
+	<div class={ nearby_container }>
 		<slot/>
 	</div>
 </div>
