50bc58eaa66ac60c13ac2ec77b9492631740b551
Johnny Hauser <johnnyhauser@gmail.com>
2021-02-07 12:00:13 -0600

    bugfix style not being reactive

--- src/Column.svelte
+++ src/Column.svelte
@@ -3,14 +3,12 @@
 	import { layout_y, layout_y_style, spacing_y_context, layout_y_child } from './layout'
 
 	export let ref = undefined
-	const { class: input_class_name, ...rest_props } = $$restProps
-	const class_name = [ input_class_name || '', 'column' ].join(' ')
 </script>
 
 <Layout
 	bind:ref
-	{ ...rest_props }
-	class={ class_name }
+	{ ...$$restProps }
+	class={ [ $$props.class || '', 'column' ].join(' ') }
 	layout_class={ layout_y }
 	layout_style={ layout_y_style }
 	layout_spacing={ spacing_y_context }
