a2eb1caa2eab04cda044cafe0b721eb62ef1fce7
Johnny Hauser <johnnyhauser@gmail.com>
2021-02-24 09:40:30 -0600

    flex-basis: 0 was disastrous in Safari, make that auto and use flex-shrink for image

--- src/image.js
+++ src/image.js
@@ -1,6 +1,8 @@
 import { css } from '@linaria/core'
 
 export const image = css`
+	flex-shrink: 1;
+
 	& > img {
 		display: block;
 		object-fit: cover;
--- src/layout.js
+++ src/layout.js
@@ -34,8 +34,8 @@ export const layout_x_child = ({ spacing_x = 0, spacing_y = 0 } = {}) => ({ heig
 	[
 		height.base.type === 'fill' ? `height: calc(100% - ${spacing_y}px);` : '',
 		height.base.type === 'content' ? `height: auto;` : '',
-		width.base.type === 'fill' ? `flex-basis: 0; flex-grow: ${width.base.value};` : '',
-		width.base.type === 'content' ? `flex-basis: auto; flex-grow: 0;` : ''
+		width.base.type === 'fill' ? `flex-grow: ${width.base.value};` : '',
+		width.base.type === 'content' ? `flex-grow: 0;` : ''
 	].join('')
 
 // static styles for y layout
@@ -45,8 +45,8 @@ export const layout_y = css`
 
 export const layout_y_child = ({ spacing_x = 0, spacing_y = 0 } = {}) => ({ height, width }) =>
 	[
-		height.base.type === 'fill' ? `flex-basis: 0; flex-grow: ${height.base.value};` : '',
-		height.base.type === 'content' ? `flex-basis: auto; flex-grow: 0;` : '',
+		height.base.type === 'fill' ? `flex-grow: ${height.base.value};` : '',
+		height.base.type === 'content' ? `flex-grow: 0;` : '',
 		width.base.type === 'fill' ? `width: calc(100% - ${spacing_x}px);` : '',
 		width.base.type === 'content' ? `width: auto;` : ''
 	].join('')
